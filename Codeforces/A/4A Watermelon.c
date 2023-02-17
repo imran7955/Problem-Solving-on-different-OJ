2539f2fc144932e3add400db990b8cafe97036e6
imran7955 <imranmansur217@gmail.com>
2023-02-17 22:57:28 +0600

    2nd program added

---
+++ Codeforces/A/4A Watermelon.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	int w;
+	scanf("%d",&w);
+	if(w==2)
+	{
+	    printf("NO\n");
+	}
+	else if(w%2==0)
+	{
+		printf("YES");
+	}
+	else
+	{
+		printf("NO\n");
+	}
+	return 0;
+}
