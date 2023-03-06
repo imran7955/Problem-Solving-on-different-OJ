2b3714a453fe11623030d527aae7815d32664533
imran7955 <imranmansur217@gmail.com>
2023-03-06 21:57:59 +0600

    Solved on 24.02.2023

---
+++ Toph/1 Contest/Intra DUET Programming Contest (Beginner) - 2023 Mock/A. Welcome to DUETCS.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+using namespace std;
+
+int main() {
+    fastio;
+    cout << "I am a good Programmer" << endl;
+    return 0;
+}
---
+++ Toph/1 Contest/Intra DUET Programming Contest (Beginner) - 2023 Mock/Readme.md
@@ -0,0 +1,3 @@
+Contest: https://toph.co/contests/training/ghnvhka
+
+Date 24.02.2023
